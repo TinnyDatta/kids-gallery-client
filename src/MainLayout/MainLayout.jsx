76f78a25eeaaa8cfd51d1f3e6a434776b3972330
TinnyDatta <tinnydatta.prof@gmail.com>
2024-08-16 00:22:46 +0600

    added basic navbar

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import Navbar from "../component/Navbar/Navbar";
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className="max-w-6xl mx-auto">
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <ToastContainer></ToastContainer>
         </div>

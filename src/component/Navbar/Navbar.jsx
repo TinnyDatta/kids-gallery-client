22d4596d69796b9fefbce2244c477c0ae2d92b49
TinnyDatta <tinnydatta.prof@gmail.com>
2024-08-16 10:05:16 +0600

    added navlink

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
           <div className="navbar bg-base-100">
   <div className="navbar-start">
-    {/* <div className="dropdown">
+    <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -22,37 +22,19 @@ const Navbar = () => {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        <li><Link to='/'>Products</Link></li>
       </ul>
-    </div> */}
+    </div>
     <a className="btn btn-ghost text-xl font-bold">Kids Gallery</a>
   </div>
-  {/* <div className="navbar-center hidden lg:flex">
+  <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+      <li><Link to='/' className="font-bold">Products</Link></li>
     </ul>
-  </div> */}
+  </div>
   <div className="navbar-end">
-    <Link to='/login' className="btn mr-3 text-lg">Login</Link>
-    <Link to='/register' className="btn text-lg">Register</Link>
+    <Link to='/login' className="btn mr-3 font-bold">Login</Link>
+    <Link to='/register' className="btn font-bold">Register</Link>
   </div>
 </div>  
     );

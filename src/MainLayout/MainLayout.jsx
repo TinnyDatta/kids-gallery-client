7311adc4de59dc5e79b0570175d6ae03fb13a9b1
TinnyDatta <tinnydatta.prof@gmail.com>
2024-08-17 12:18:07 +0600

    added footer

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Navbar from "../component/Navbar/Navbar";
+import Footer from "../component/Footer/Footer";
 
 const MainLayout = () => {
     return (
         <div className="max-w-6xl mx-auto">
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
             <ToastContainer></ToastContainer>
         </div>
     );
